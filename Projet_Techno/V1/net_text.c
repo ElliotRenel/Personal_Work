cb893ad738e388177917d53fc0bb650b1ff81386
ElliotLinux <elliot.renel@etu.u-bordeaux.fr>
2018-09-27 11:24:29 +0200

    supression de la déclaration de uint (déjà déclaré sur Linux)

--- Projet_Techno/V1/net_text.c
+++ Projet_Techno/V1/net_text.c
@@ -3,8 +3,6 @@
 #include "game.h"
 #include "game_io.h"
 
-typedef unsigned long uint;
-
 int main(void){
   piece pe[25]={LEAF,LEAF,LEAF,TEE,CORNER,SEGMENT,CORNER,TEE,CORNER,LEAF,TEE,TEE,SEGMENT,LEAF,LEAF,LEAF,TEE,SEGMENT,SEGMENT,CORNER,LEAF,TEE,SEGMENT,SEGMENT,LEAF};
   direction dir[25]={N,N,E,N,W,N,E,N,S,S,E,N,E,W,N,S,E,E,E,S,E,S,W,W,W};

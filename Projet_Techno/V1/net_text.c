043e8f8b2a0bc21930e8d1593eef81f76fd4068b
ElliotRenel <elliot.renel@etu.u-bordeaux.fr>
2018-09-28 21:30:41 +0200

    Avancement de net_text.c

--- Projet_Techno/V1/net_text.c
+++ Projet_Techno/V1/net_text.c
@@ -3,11 +3,20 @@
 #include "game.h"
 #include "game_io.h"
 
-int main(void){
+game default_game(cgame* soluce){
   piece pe[25]={LEAF,LEAF,LEAF,TEE,CORNER,SEGMENT,CORNER,TEE,CORNER,LEAF,TEE,TEE,SEGMENT,LEAF,LEAF,LEAF,TEE,SEGMENT,SEGMENT,CORNER,LEAF,TEE,SEGMENT,SEGMENT,LEAF};
   direction dir[25]={N,N,E,N,W,N,E,N,S,S,E,N,E,W,N,S,E,E,E,S,E,S,W,W,W};
-  cgame soluce = new_game(pe, dir);
-  game g = (game)soluce;
+  *(soluce) = new_game(pe, dir);
+  game g = (game)(*soluce);
   shuffle_dir(g);
+  return g;
+}
+
+int main(void){
+
+  cgame sol = new_game_empty();
+  game g = default_game(&sol);
+  printf("%d",game_width(g));
+
   return EXIT_SUCCESS;
 }

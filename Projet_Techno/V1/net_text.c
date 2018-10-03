936c574de6aa4018e2467a27a115c46bf5ed78e8
ElliotRenel <elliot.renel@etu.u-bordeaux.fr>
2018-10-03 14:55:25 +0200

    Début de la fonction afficher_game

--- Projet_Techno/V1/net_text.c
+++ Projet_Techno/V1/net_text.c
@@ -3,6 +3,13 @@
 #include "game.h"
 #include "game_io.h"
 
+/*
+char leafs[4]={'^','>','v','<'};
+char tees[4]={'┴','├','┬','┤'};
+char corners[4]={'└','','┐','┘'};
+char segments[4]={'|','-','|','-'};
+*/
+
 game default_game(cgame* soluce){
   piece pe[25]={LEAF,LEAF,LEAF,TEE,CORNER,SEGMENT,CORNER,TEE,CORNER,LEAF,TEE,TEE,SEGMENT,LEAF,LEAF,LEAF,TEE,SEGMENT,SEGMENT,CORNER,LEAF,TEE,SEGMENT,SEGMENT,LEAF};
   direction dir[25]={N,N,E,N,W,N,E,N,S,S,E,N,E,W,N,S,E,E,E,S,E,S,W,W,W};
@@ -12,11 +19,26 @@ game default_game(cgame* soluce){
   return g;
 }
 
+/*
+void afficher_game(game g){
+  int H = game_height((cgame)g);
+  int W = game_width((cgame)g);
+  int i;
+  char ligne[W];
+  for (i=H*W-1; i>0; i--){
+    if(i%W==W-1){
+      printf("%s\n",ligne);
+    }
+    ligne[i%W];
+  }
+}
+*/
+
 int main(void){
 
   cgame sol = new_game_empty();
   game g = default_game(&sol);
-  printf("%u\n",game_width(g));
+  printf("%u",sizeof(g));
 
   return EXIT_SUCCESS;
 }

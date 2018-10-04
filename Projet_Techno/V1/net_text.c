544975406e30683418d2fcb4461279500796c03e
ElliotLinux <elliot.renel@etu.u-bordeaux.fr>
2018-10-04 17:57:01 +0200

    net_text.c marche du tonnere !!

--- Projet_Techno/V1/net_text.c
+++ Projet_Techno/V1/net_text.c
@@ -4,6 +4,7 @@
 #include "game.h"
 #include "game_io.h"
 
+#define clear() printf("\033[H\033[J")
 
  char leafs[4]={'^','>','v','<'};
  char segments[4] = {'|','-','|','-'};
@@ -24,9 +25,9 @@ game default_game(cgame* soluce){
 void afficher_game(cgame g){
   int H = game_height(g);
   int W = game_width(g);
-  printf("\n\n");
+  printf("\n\n\t   0 1 2 3 4\n");
   for(int y=H-1; y>=0; y--){
-    printf("|\t\t");
+    printf("|\t%d ",y);
     for(int x=0; x<W; x++){
       piece p = get_piece(g, x, y);
       direction d = get_current_dir(g, x, y);
@@ -40,7 +41,7 @@ void afficher_game(cgame g){
         printf(" %s",tees[d]);
       }
     }
-    printf("\t\t|\n");
+    printf(" \t|\n");
   }
   printf("\n\n");
 }
@@ -49,7 +50,7 @@ void afficher_game(cgame g){
 int main(void){
   cgame sol = new_game_empty();
   game g = default_game(&sol);
-
+  clear();
   while(!is_game_over ((cgame) g)){
     afficher_game((cgame)g);
     int x, y;
@@ -57,9 +58,22 @@ int main(void){
     scanf("%d %d",&x,&y);
     if(!(x<0 || y<0 || x>=game_width((cgame)g) || y>=game_height((cgame)g))){
       rotate_piece_one(g, x, y);
+    }else{
+      clear();
+      printf("\n\n\nPosition invalide, essaie encore\n");
+      sleep(1);
+      for (int i = 4; i>0; i--){
+        printf(".\n");
+        sleep(1);
+      }
     }
+    clear();
   }
   afficher_game((cgame)g);
-  
+
+  printf("\n\nBravo : tu as réussi !\n\n");;
+  sleep(5);
+  clear();
+
   return EXIT_SUCCESS;
 }

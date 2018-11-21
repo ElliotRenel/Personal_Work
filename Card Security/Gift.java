fc169abea21bb4539efda493b7928460b4c95c0c
ElliotRenel <elliot.renel@etu.u-bordeaux.fr>
2018-11-21 20:56:17 +0100

    Implementation beginning

---
+++ Card Security/Gift.java
@@ -0,0 +1,13 @@
+
+
+class Gift{
+    String giver;
+    String receiver;
+    long code;
+    char check;
+    
+
+    public static void main(String[] args){
+        System.out.println("bruh");
+    }
+}
